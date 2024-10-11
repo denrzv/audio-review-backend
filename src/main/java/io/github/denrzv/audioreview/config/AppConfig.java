add9c1916415c4d7eebe516e2fdb01858f0cfc99
denrzv <voofaa@gmail.com>
2024-10-11 11:01:40 +0300

    feat(search, reclassify):  added file search, reclassification

---
+++ src/main/java/io/github/denrzv/audioreview/config/AppConfig.java
@@ -0,0 +1,12 @@
+package io.github.denrzv.audioreview.config;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Slf4j
+@Configuration
+public class AppConfig {
+
+}
